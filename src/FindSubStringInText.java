94946f4f515abeecd66f60a39c26b4797f659695
Al-Rou <roal.24.alro@gmail.com>
2021-07-05 21:31:30 -0400

    Commented

--- src/FindSubStringInText.java
+++ src/FindSubStringInText.java
@@ -4,23 +4,34 @@ import java.util.List;
 public class FindSubStringInText {
     public String showSubString(String text, String sub)
     {
+        //This list separates and stores the entered text into characters
         List<Character> list = new ArrayList<>();
+        //This list stores the indexes of the above mentioned list where the sub string starts inside the text
         List<Integer> resultList = new ArrayList<>();
+        //Fill in the list with characters of the entered text
         for(int i = 0; i < text.length(); i++)
         {
             list.add(i, text.charAt(i));
         }
         String auxiliary = "";
         int sizeOfList = list.size();
+        //The outer loop goes on the list
         for(int j = 0; j < sizeOfList; j++) {
+            //Every time, a string is created from the beginning of the list
             for (int i = 0; i < sub.length(); i++) {
                 auxiliary += list.get(i);
             }
+            //It is checked whether the created string equals with the sub string.
+            //If yes, the beginning index is stored in resultList.
             if (auxiliary.equals(sub)) {
                 resultList.add(j);
             }
+            //The auxiliary is reset to go back in the loop!
             auxiliary = "";
+            //One character from the head of the list is removed
             list.remove(0);
+            //Check whether the size of list has gone under the size of the sub string.
+            //If yes, the loop must be broken, otherwise an error (boundary index exception) pops up!
             if(list.size() < sub.length())
             {
                 break;
